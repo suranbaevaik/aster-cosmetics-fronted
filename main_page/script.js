f790adacde188ea033407e8adae02c866ee985ed
Aikanysh <suranbaeva.aik@gmail.com>
2023-01-10 18:05:25 +0600

    init: start

---
+++ main_page/script.js
@@ -0,0 +1,55 @@
+/*const h1 = document.getElementsByTagName('h1');
+console.log(h1)
+
+const logo = document.getElementById('logo');
+
+logo.setAttribute('src', 'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg');
+console.log(logo);*/
+
+/*const btn = document.createElement('button');
+const homeElement = document.querySelector('.products-container');
+homeElement.append(btn);
+btn.textContent = 'knopka'
+
+homeElement.insertAdjacentElement('afterend', btn);
+
+btn.classList.add('btn-shop', 'buy-all-btn');
+btn.classList.remove('buy-all-btn');
+
+const checkClass = btn.classList.contains('btn-shop');
+console.log(checkClass)
+
+btn.setAttribute('type', 'submit');*/
+
+
+/*const addElement = confirm('Хотите добавить элемент?');
+const el = document.querySelector('body');
+
+if (addElement) {
+    const img = document.createElement('img');
+    img.setAttribute('src', 'https://exoplanets.nasa.gov/internal_resources/1806');
+    img.classList.add('main-image');
+    img.style.marginTop = '20px';
+
+    el.append(img);
+} else {
+    const link = document.createElement('a');
+    link.setAttribute('href', 'https://google.com');
+    link.setAttribute('target', '_blank');
+    link.classList.add('main-link');
+    link.style.marginTop = '20px';
+    link.style.textDecoration = 'none';
+    link.textContent = 'GOOGLE';
+
+    el.append(link);
+}*/
+
+/*const btn = document.querySelector('.btn-shop');
+let counter = 0;
+
+btn.addEventListener('click', () => {
+    counter++;
+    alert(counter);
+})*/
+
+
